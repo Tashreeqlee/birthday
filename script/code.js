e48678468e2a04c2fd7d0f183dc87c55ae06ee70
Tashreeqlee <146819122+Tashreeqlee@users.noreply.github.com>
2023-11-20 19:30:53 +0200

    jsAdded

--- script/code.js
+++ script/code.js
@@ -1,1 +1,14 @@
-//
+function calculateDaysUntilBirthday() {
+    var name = document.getElementById("nameInput").value;
+    var birthdate = new Date(document.getElementById("birthdateInput").value);
+    var today = new Date();
+    var nextBirthday = new Date(today.getFullYear(), birthdate.getMonth(), birthdate.getDate());
+
+    if (nextBirthday < today) {
+        nextBirthday.setFullYear(nextBirthday.getFullYear() + 1);
+    }
+
+    var daysUntilBirthday = Math.ceil((nextBirthday - today) / (1000 * 60 * 60 * 24));
+
+    document.getElementById("output").innerHTML = name + ", your next birthday is in " + daysUntilBirthday + " days.";
+}
